b7e7f8857ce8948f98f3ce90b0469d297e80cf55
Hair_Trigg3r <tylerjreimer@gmail.com>
2023-02-26 13:06:00 -0500

    Add skeleton GET routes for each view

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,16 +1,37 @@
-const router = require('express').Router();
-const { User } = require('../models');  // will need ot add other models
-const withAuth = require('../utils/auth'); // not sure if this wil be needed here
+// Should really be called 'getRoutes'
 
+const router = require('express').Router();
+const { User, Entry, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+// Renders login/signup view
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/');  // this may need to change
+    res.redirect('/home');
     return;
   }
 
   res.render('login');
 });
 
-// do a 404 page '*' route
+// Renders the homepage (all entries from everyone)
+router.get('/home', (res, res) => {
+  // get and serialize ALL entries (just entries, no comment inclusion)
+  res.render('home');   // need to pass in the data and 'logged_in'
+});
+
+// Renders the signed in user's dashboard
+router.get('/dashboard/:userId', (req, res) => {  //pass in withAtuh middleware
+  // get and serialze this users entries
+  res.render('dashboard');  // need to pass in the data and 'logged_in'
+});
+
+// Renders one of a user's entries, along with the associated comments
+router.get('/entry/:userId/:entryId', (req, res) => {     //dont need to use withAuth here
+  // get and serialize this one entry, and assoc. comments
+  res.render('blogentry'); // need to pass in all that data and 'logged_in'
+});
+
+// a 404 page '*' route
 
 module.exports = router;
