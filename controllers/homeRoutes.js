8839b8713274e97c1b74f9241b2b0cf1e7ab6fde
Hair_Trigg3r <tylerjreimer@gmail.com>
2023-02-27 16:12:18 -0500

    Add functionality to GET one entry route & view

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -19,29 +19,33 @@ router.get('/login', (req, res) => {
 
 // Renders the homepage (all entries from everyone)
 router.get('/', async (req, res) => {
-  // Grab all entries, with associated user's name
-  const entryData = await Entry.findAll({
-    include: [
-      {
-        model: User,
-        attributes: ['name'],
-      },
-    ],
-  });
+  try {
+    // Grab all entries, with associated user's name
+    const entryData = await Entry.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
 
-  // Serialize
-  const entries = entryData.map((project) => project.get({ plain: true }));
+    // Serialize
+    const entries = entryData.map((project) => project.get({ plain: true }));
 
-  // Pass data into render
-  res.render('home', {
-    entries,
-    logged_in: req.session.logged_in,
-    username: req.session.username
-  });
+    // Pass data into render
+    res.render('home', {
+      entries,
+      logged_in: req.session.logged_in,
+      username: req.session.username
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // Renders the signed in user's dashboard
-router.get('/dashboard/:username', (req, res) => {  //pass in withAtuh middleware
+router.get('/dashboard/:username', withAuth, async (req, res) => {
   // get and serialze this users entries
   res.render('dashboard', {
     logged_in: req.session.logged_in,
@@ -50,12 +54,34 @@ router.get('/dashboard/:username', (req, res) => {  //pass in withAtuh middlewar
 });
 
 // Renders one of a user's entries, along with the associated comments
-router.get('/entry/:username/:entryId', (req, res) => {     //dont need to use withAuth here
-  // get and serialize this one entry, and assoc. comments
-  res.render('blogentry', {
-    logged_in: req.session.logged_in,
-    username: req.session.username
-  }); // need to pass in all that data and 'logged_in'
+
+// ! Currently doesnt include comments!
+router.get('/entry/:username/:entryId', async (req, res) => {
+  // Grab entry by its id (via params)
+  try {
+    const entryData = await Entry.findByPk(req.params.entryId, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize
+    const entry = entryData.get({ plain: true });
+
+    console.log(entry);
+
+    // Pass data into render
+    res.render('blogentry', {
+      ...entry,
+      logged_in: req.session.logged_in,
+      username: req.session.username
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // a 404 page '*' route
