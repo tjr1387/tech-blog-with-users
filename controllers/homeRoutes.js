e7c0e8b85157d7623c10a274fc42548091336312
Hair_Trigg3r <tylerjreimer@gmail.com>
2023-02-27 14:22:59 -0500

    Tweak endpoints for login/signup actions

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,29 +7,29 @@ const withAuth = require('../utils/auth');
 // Renders login/signup view
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/home');
+    res.redirect('/');
     return;
   }
 
-  res.render('login');
+  res.render('login', { logged_in: req.session.logged_in });
 });
 
 // Renders the homepage (all entries from everyone)
 router.get('/', (req, res) => {
   // get and serialize ALL entries (just entries, no comment inclusion)
-  res.render('home');   // need to pass in the data and 'logged_in'
+  res.render('home', { logged_in: req.session.logged_in });   // need to pass in the data and 'logged_in'
 });
 
 // Renders the signed in user's dashboard
 router.get('/dashboard/:userId', (req, res) => {  //pass in withAtuh middleware
   // get and serialze this users entries
-  res.render('dashboard');  // need to pass in the data and 'logged_in'
+  res.render('dashboard', { logged_in: req.session.logged_in });  // need to pass in the data and 'logged_in'
 });
 
 // Renders one of a user's entries, along with the associated comments
 router.get('/entry/:userId/:entryId', (req, res) => {     //dont need to use withAuth here
   // get and serialize this one entry, and assoc. comments
-  res.render('blogentry'); // need to pass in all that data and 'logged_in'
+  res.render('blogentry', { logged_in: req.session.logged_in }); // need to pass in all that data and 'logged_in'
 });
 
 // a 404 page '*' route
